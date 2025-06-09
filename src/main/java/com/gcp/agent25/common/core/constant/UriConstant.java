40e88c59e78e02aa3571eea2269dda9f3fba3733
Brandon Tan <brandontan2003@gmail.com>
2025-06-09 10:15:42 +0800

    Add UriConstant

--- src/main/java/com/gcp/agent25/common/core/constant/UriConstant.java
+++ src/main/java/com/gcp/agent25/common/core/constant/UriConstant.java
@@ -5,6 +5,8 @@ public class UriConstant {
     public static final String API_VERSION_1 = "/v1";
 
     // Case Management Service URI
-    public static final String API_SUPPORT = "/api/case";
+    public static final String API_CASE = "/api/case";
+    public static final String TICKET_URL = "/ticket";
+    public static final String TICKETS_URL = "/tickets";
 
 }
